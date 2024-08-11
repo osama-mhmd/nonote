50f72270251471f32cffca29f61e44cdb844fade
os-mhmd <docosmo010@gmail.com>
2024-08-11 21:58:35 +0300

    chore/identity-for-the-project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,10 @@ export default function Home() {
   return (
     <main className="flex flex-col pt-28">
       <div className="container flex flex-col items-center text-center gap-4">
-        <h1 className="text-4xl sm:text-6xl font-bold">My project</h1>
+        <h1 className="text-4xl sm:text-6xl font-bold">Nonote</h1>
         <p className="text-muted-foreground max-w-prose">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, laborum
-          tempore, sint magni ex repellendus inventore doloribus laboriosam
-          cupiditate, vero porro incidunt nulla voluptas laudantium tenetur
-          dolore dolores nobis quod.
+          Everyday you see a new note taking app appears, but they all don{"'"}t
+          fit you. Don{"'"}t worry, this app will fit you
         </p>
         <div className="flex flex-wrap items-center gap-2">
           <Button variant="outline" asChild>
