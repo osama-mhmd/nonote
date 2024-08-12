aee0597ca038b638436818d7aec2075b1a02d464
os-mhmd <docosmo010@gmail.com>
2024-08-12 12:44:40 +0300

    feat/ux-enhanced-by-adding-icons

--- src/app/auth/profile/page.tsx
+++ src/app/auth/profile/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Edit, LogOut } from "lucide-react";
 import Image from "next/image";
 
 export default function Profile() {
@@ -18,13 +19,17 @@ export default function Profile() {
           className="rounded-full bg-muted mx-auto"
           src="/test.png"
         />
-        <p className="rounded-md bg-muted my-2 p-3">Name: Osama Mohammed</p>
-        <p className="rounded-md bg-muted my-2 p-3">Username: os-mhmd</p>
-        <p className="rounded-md bg-muted my-2 p-3">
-          Email: docosmo010@gmail.com
+        <p className="rounded-md bg-muted my-2 p-3 flex items-center gap-1">
+          Name: Osama Mohammed <Edit height={20} width={20} />
+        </p>
+        <p className="rounded-md bg-muted my-2 p-3 flex items-center gap-1">
+          Username: os-mhmd <Edit height={20} width={20} />
+        </p>
+        <p className="rounded-md bg-muted my-2 p-3 flex items-center gap-1">
+          Email: docosmo010@gmail.com <Edit height={20} width={20} />
         </p>
         <Button variant="destructive" onClick={logOut}>
-          Log out
+          Log out <LogOut className="ms-2" />
         </Button>
       </div>
     </section>
