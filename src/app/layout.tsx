dd1980d27b812a4e24a2ae3381cb3b1916458fea
osama-mhmd <docosmo010@gmail.com>
2024-08-27 16:36:16 +0200

    refactor/restructure-style-files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import Nav from '@/components/nav'
+import "@/styles/globals.css";
+import Nav from "@/components/nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Nav></Nav>
-        {children}</body>
+        {children}
+      </body>
     </html>
   );
 }
