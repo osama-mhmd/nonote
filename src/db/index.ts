a3e55187cbb821bb29a6cbb04bfc40f555b50fdd
osama-mhmd <docosmo010@gmail.com>
2024-09-04 09:19:49 +0200

    fix/quick

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-const dbURI =
-  "postgresql://postgres.aoihtskteljdmvrizgwd:0jmXPjR2po3PiZqw@aws-0-eu-central-1.pooler.supabase.com:6543/postgres";
+const dbURI = process.env.DATABASE_URL!;
 
-const client = postgres(dbURI!, {
+const client = postgres(dbURI, {
   ssl: {
     rejectUnauthorized: false,
   },
