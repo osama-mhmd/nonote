8243449bf6aaea1352bb6129144b15b567de9c4c
osama-mhmd <docosmo010@gmail.com>
2024-09-11 17:59:26 +0200

    hotfix: where selector

--- src/db/utils/new-verify.ts
+++ src/db/utils/new-verify.ts
@@ -4,12 +4,23 @@ import { generateEmailVerificationCode } from "./verification-code";
 import { userTable } from "../schemas";
 import db from "..";
 import sendMail from "./send-mail";
+import { eq } from "drizzle-orm";
+import { validateRequest } from "../auth";
 
 export async function newVerify(email: string) {
+  const { user } = await validateRequest();
+
+  if (!user)
+    return {
+      message: "unauthorized",
+    };
+
   const { verificationCode, expiresAt } = generateEmailVerificationCode();
   await db
     .update(userTable)
-    .set({ emailVerified: `${expiresAt}=${verificationCode}` });
+    .set({ emailVerified: `${expiresAt}=${verificationCode}` })
+    .where(eq(userTable.id, user.id));
+
   await sendMail(
     email,
     "Please verify your email",
