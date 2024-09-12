26f3c33c86502a95b539f6da69b8044689d6ecb6
osama-mhmd <docosmo010@gmail.com>
2024-09-12 16:07:32 +0200

    feat: checking for public visibility

--- src/app/app/workspace/[workspace_id]/page.tsx
+++ src/app/app/workspace/[workspace_id]/page.tsx
@@ -1,26 +1,37 @@
 import { getWorkspace } from "@/db/workpace-actions/get-workspaces";
-import permission from "@/db/workpace-actions/permission";
+import permission, { Permission } from "@/db/workpace-actions/permission";
 
 export default async function Space({
   params,
 }: {
   params: { workspace_id: string };
 }) {
-  const userPermission = await permission(params.workspace_id);
+  let userPermission = await permission(params.workspace_id);
+  const workspace = await getWorkspace(params.workspace_id);
 
-  if (userPermission == "not-found")
+  if (userPermission == "not-found") {
     return (
       <h3 className="text-center">
         Sorry, this workspace doesn{"'"}t exist 😞
       </h3>
     );
+  }
 
-  if (userPermission == "no-access")
-    return (
-      <h3 className="text-center">You have no access for this workspace 😞</h3>
-    );
-
-  const workspace = await getWorkspace(params.workspace_id);
+  if (userPermission == "no-access") {
+    // checking for visibility
+    const visibility = workspace.visibility.split("public-");
+    if (visibility.length == 1) {
+      // this means that workspace.visibility is equal to "private"
+      return (
+        <h3 className="text-center">
+          You have no access for this workspace 😞
+        </h3>
+      );
+    } else {
+      // this means that workspace.visibility is equal is "public-(edit|view|comment)"
+      userPermission = visibility[1] as Permission;
+    }
+  }
 
   return (
     <section className="mt-6 sm:mt-12">
