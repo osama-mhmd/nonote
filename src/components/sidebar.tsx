1c286d04d9db5d47884f82ad73ce3d3b6234636e
osama-mhmd <docosmo010@gmail.com>
2024-09-13 09:05:31 +0200

    feat: sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,29 +1,155 @@
-import { Home } from "lucide-react";
-import Link from "next/link";
-import { Button } from "./ui/button";
+"use client";
 
-export default function Sidebar() {
+import React, { ElementRef, useEffect, useRef, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+
+import { Bolt, ChevronsLeft, House, MenuIcon, Settings } from "lucide-react";
+
+const Sidebar = () => {
+  const pathname = usePathname();
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
+  const isResizingRef = useRef(false);
+  const sidebarRef = useRef<ElementRef<"aside">>(null);
+  const navbarRef = useRef<ElementRef<"div">>(null);
+  const [isResetting, setIsResetting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    } else {
+      resetWidth();
+    }
+  }, [isMobile]);
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    }
+  }, [pathname, isMobile]);
+
+  const handleMouseDown = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    isResizingRef.current = true;
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  };
+
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!isResizingRef.current) return;
+    let newWidth = e.clientX;
+
+    if (newWidth < 240) newWidth = 240;
+    if (newWidth > 480) newWidth = 480;
+
+    if (sidebarRef.current && navbarRef.current) {
+      sidebarRef.current.style.width = `${newWidth}px`;
+      navbarRef.current.style.setProperty("left", `${newWidth}px`);
+      navbarRef.current.style.setProperty(
+        "width",
+        `calc(100% - ${newWidth}px)`,
+      );
+    }
+  };
+
+  const handleMouseUp = () => {
+    isResizingRef.current = false;
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
+  };
+
+  const resetWidth = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(false);
+      setIsResetting(true);
+
+      sidebarRef.current.style.width = isMobile ? "100%" : "240px";
+      navbarRef.current.style.removeProperty("width");
+      navbarRef.current.style.setProperty(
+        "width",
+        isMobile ? "0" : "calc(100%-240px)",
+      );
+      navbarRef.current.style.setProperty("left", isMobile ? "100%" : "240px");
+      setTimeout(() => setIsResetting(false), 300);
+    }
+  };
+
+  const collapse = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(true);
+      setIsResetting(true);
+
+      sidebarRef.current.style.width = "0";
+      navbarRef.current.style.setProperty("width", "100%");
+      navbarRef.current.style.setProperty("left", "0");
+      setTimeout(() => setIsResetting(false), 300);
+    }
+  };
+
   return (
-    <div className="flex h-screen w-16 flex-col justify-between border-e bg-white">
-      <div>
-        <div className="inline-flex size-16 items-center justify-center">
-          <span className="grid size-10 place-content-center rounded-lg bg-gray-200 text-xs text-gray-600">
-            L
-          </span>
+    <>
+      <aside
+        ref={sidebarRef}
+        className={cn(
+          "group/sidebar px-3 relative z-[300] flex h-screen w-60 flex-col overflow-y-auto bg-secondary",
+          isResetting && "transition-all duration-300 ease-in-out",
+          isMobile && "w-0",
+        )}
+      >
+        <div
+          onClick={collapse}
+          role="button"
+          className={cn(
+            "absolute right-2 top-3 h-6 w-6 rounded-sm text-muted-foreground opacity-0 transition hover:bg-primary/10 group-hover/sidebar:opacity-100",
+            isMobile && "opacity-100",
+          )}
+        >
+          <ChevronsLeft className="h-6 w-6" />
         </div>
-
-        <div className="border-t border-gray-100">
-          <div className="px-2">
-            <div className="py-4 flex justify-center">
-              <Button variant="ghost" asChild>
-                <Link href="#">
-                  <Home />
-                </Link>
-              </Button>
-            </div>
-          </div>
+        <div className="p-4 px-6 cursor-pointer rounded-md bg-primary/20 w-fit mt-6 mb-2">
+          L
         </div>
+        <div className="flex gap-2 cursor-pointer hover:bg-gray-200 items-center py-2 px-3 rounded-md transition">
+          <House width={20} /> Home
+        </div>
+        <div
+          onMouseDown={handleMouseDown}
+          onClick={resetWidth}
+          className="absolute right-0 top-0 h-full w-1 cursor-ew-resize bg-primary/10 opacity-0 transition group-hover/sidebar:opacity-100"
+        ></div>
+      </aside>
+      <div
+        ref={navbarRef}
+        className={cn(
+          "absolute left-60 top-0 z-[300] w-[calc(100%-240px)]",
+          isResetting && "transition-all duration-300 ease-in-out",
+          isMobile && "left-0 w-full",
+        )}
+      >
+        <nav
+          className={cn(
+            "w-full bg-transparent px-3 py-2",
+            !isCollapsed && "p-0",
+          )}
+        >
+          {isCollapsed && (
+            <MenuIcon
+              onClick={resetWidth}
+              role="button"
+              className="h-6 w-6 text-muted-foreground"
+            />
+          )}
+        </nav>
       </div>
-    </div>
+    </>
   );
-}
+};
+export default Sidebar;
