1e790be8aa8ae0d6915b550408c655cdd72d7170
osama-mhmd <docosmo010@gmail.com>
2024-09-13 18:38:23 +0200

    feat: task list

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,13 +4,16 @@ import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Placeholder from "@tiptap/extension-placeholder";
 import Heading from "@tiptap/extension-heading";
-
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
 import "@/styles/editor.css";
 
 const Editor = () => {
-  const editor = useEditor({
+  const titleEditor = useEditor({
     extensions: [
-      StarterKit,
+      StarterKit.configure({
+        heading: false,
+      }),
       Heading.extend({
         addKeyboardShortcuts() {
           return {
@@ -35,7 +38,26 @@ const Editor = () => {
     },
   });
 
-  return <EditorContent className="mt-16" editor={editor} />;
+  const pageEditor = useEditor({
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Placeholder.configure({
+        placeholder: "Start writing here...",
+      }),
+    ],
+    immediatelyRender: false,
+  });
+
+  return (
+    <>
+      <EditorContent className="mt-16" editor={titleEditor} />
+      <EditorContent editor={pageEditor} />
+    </>
+  );
 };
 
 export default Editor;
