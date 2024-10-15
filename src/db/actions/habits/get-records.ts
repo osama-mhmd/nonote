7c4482f8eb6a436b342396c05d4a930c5c3ee324
osama-mhmd <docosmo010@gmail.com>
2024-10-15 14:56:22 +0300

    fix: issue in selecting records

--- src/db/actions/habits/get-records.ts
+++ src/db/actions/habits/get-records.ts
@@ -3,7 +3,7 @@
 import db from "@/db";
 import { validateRequest } from "@/db/auth";
 import { habitsRecordsTable, habitsTable } from "@/db/schemas/habits";
-import { and, eq } from "drizzle-orm";
+import { and, asc, eq } from "drizzle-orm";
 
 export default async function getRecords(habit_id: string) {
   const { user } = await validateRequest();
@@ -15,8 +15,9 @@ export default async function getRecords(habit_id: string) {
       record_date: habitsRecordsTable.date,
     })
     .from(habitsRecordsTable)
-    .innerJoin(habitsTable, eq(habitsTable.user_id, user.id))
-    .where(and(eq(habitsRecordsTable.habit_id, habit_id)));
+    .innerJoin(habitsTable, eq(habitsTable.id, habitsRecordsTable.habit_id))
+    .where(and(eq(habitsTable.user_id, user.id), eq(habitsTable.id, habit_id)))
+    .orderBy(asc(habitsRecordsTable.date));
 
   return records;
 }
